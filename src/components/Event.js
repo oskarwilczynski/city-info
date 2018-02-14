a4cb9d6779bf2105a3da125d81ec73cbbf142594
oskarwilczynski <oskar.andrzej.wilczynski@gmail.com>
2018-02-14 23:59:54 +0000

    Started styling

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+const StyledDiv = styled.div`
+    && {
+        height: 20%;
+    }
+`
 
 class Event extends React.Component {
     render() {
         const imagePlaceholder = "https://www.makeupgeek.com/content/wp-content/themes/makeup-geek/images/placeholder-square.svg"
 
         return (
-            <div>
+            <StyledDiv>
                 <img src={this.props.details.image ? this.props.details.image.url : imagePlaceholder} />
                 <h3>{this.props.details.title}</h3>
                 <p>{this.props.details.venue_name}</p>
-            </div>
+            </StyledDiv>
         )
     }
 }
